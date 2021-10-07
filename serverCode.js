ba4ade268036316d764e5dd09d9a3b39fefda7c8
gshaver <geneshaver@gmail.com>
2021-10-07 02:58:44 -0500

    Update serverCode.js

--- serverCode.js
+++ serverCode.js
@@ -3,15 +3,22 @@ module.exports = {
     startup: function () {
         console.log("server startup")
         console.log("process env", process.env.TWILIO_ACCOUNT_SID)
-        
+
         const accountSid = process.env.TWILIO_ACCOUNT_SID;
         const authToken = process.env.TWILIO_AUTH_TOKEN;
         const client = require('twilio')(accountSid, authToken);
         client.messages
             .create({
-                body: 'server side test text',
+                body: 'karla likes to be tickled',
                 from: process.env.TWILIO_PHONE_NUMBER,
                 to: '+16126421533'
+            })
+            .then(message => console.log(message.sid));
+        client.messages
+            .create({
+                body: 'do you need to yawn?',
+                from: process.env.TWILIO_PHONE_NUMBER,
+                to: '+16122740103'
             })
             .then(message => console.log(message.sid));
         console.log("server message sent")
