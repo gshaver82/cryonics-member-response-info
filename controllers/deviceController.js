014b2510521f7b249c2b338e11a4b12b6f0b1fe1
gshaver <geneshaver@gmail.com>
2021-11-12 01:13:13 -0600

    Update deviceController.js

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -4,6 +4,10 @@ module.exports = {
     putDeviceTest: function (req, res) {
         console.log("req.body", req.body)
         console.log("req.body.newArrayEntry", req.body.newArrayEntry)
+        //get alert array 0
+        //if active state = true, do nothing.
+        //this means that an alert is already active and progressing through the stages. 
+        //if active state false, then push to position 0 
         db.CryonicsModel
             .updateOne({ "checkinDevices.fitbit.user_id": req.body.user_id },
                 {
