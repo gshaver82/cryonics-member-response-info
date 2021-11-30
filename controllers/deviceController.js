5544ac3d13fe43c0db47f96eeebc06ee2b1dd59e
gshaver <geneshaver@gmail.com>
2021-11-30 16:34:10 -0600

    Update deviceController.js

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -52,8 +52,8 @@ module.exports = {
             .updateOne({ _id: req.params._id },
                 {
                     $set: {
-                        "checkinDevices.fitbit.syncAlertArray.0.activeState": false,
-                        "checkinDevices.fitbit.alertArray.0.activeState": false,
+                        "checkinDevices.fitbit.syncAlertArray.$.activeState": false,
+                        "checkinDevices.fitbit.alertArray.$.activeState": false,
                     }
                 }
             )
