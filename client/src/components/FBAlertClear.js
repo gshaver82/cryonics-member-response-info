c27748892b035b4301353aa255a29030edd56611
gshaver <geneshaver@gmail.com>
2021-11-30 17:36:00 -0600

    Update FBAlertClear.js

--- client/src/components/FBAlertClear.js
+++ client/src/components/FBAlertClear.js
@@ -9,24 +9,41 @@ function ProfileDetails() {
   const { _id } = useParams()
   const [isLoading, setisLoading] = useState(true);
   const [AlertResponse, setAlertResponse] = useState(0);
+  const [syncAlertResponse, setsyncAlertResponse] = useState(0);
 
   useEffect(() => {
+
     const response = API.putClearFBAlert(_id)
-      .then(res => setAlertResponse(res.data))
+      .then(res => setAlertResponse(res.data.nModified))
+      .catch(err => console.log(err));
+    console.log("api response", response)
+
+    const syncresponse = API.putClearFBSyncAlert(_id)
+      .then(res => setsyncAlertResponse(res.data.nModified))
       .then(setisLoading(false))
       .catch(err => console.log(err));
-    console.log("response", response)
+    console.log("api  syncresponse", syncresponse)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log("AlertResponse", AlertResponse)
+  console.log("state AlertResponse 0 for not mod, 1 for mod", AlertResponse)
+  console.log("state syncAlertResponse 0 for not mod, 1 for mod", syncAlertResponse)
   return (
 
     <div>
       <h2>Profile Details{isLoading && <span>please wait, loading the data now.</span>}</h2>
-      <p>Alert state cleared</p>
+      <p>This page will attempt to clear alerts from either fitbit watch notifications, or fitbit heart rate sync info going dark</p>
       <p>If it hasnt already, the Fitbit watch will automatically resume monitoring
         when it detects heart rate after it sent the alert</p>
+      {AlertResponse === 0
+        ?
+        <p>Alert not yet cleared</p>
+        : <p>Alert cleared</p>
+      }
+      {syncAlertResponse === 0
+        ? <p>Alert not yet cleared</p>
+        : <p>Alert cleared</p>
+      }
     </div>
   );
 }
