3238a4a1ace1bec4c96b884fbbdfc461b56af545
gshaver <geneshaver@gmail.com>
2021-11-30 20:44:39 -0600

    Update deviceController.js

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -46,44 +46,44 @@ module.exports = {
         res.json({ update, user })
     },
 
-    // putClearFBAlert: async function (req, res) {
-    //     let user = 0
-    //     console.log("putClearFBAlert req.params._id", req.params._id)
-    //     // try {
-    //     //     user = await db.CryonicsModel.findOne({ _id: req.params._id }).exec()
-    //     // } catch (err) {
-    //     //     return res.status(400).json({
-    //     //         error: 'Error en STATUS2'
-    //     //     })
-    //     // }
-    //     // console.log("putClearFBAlert   --------  user.name", user.name)
-    //     // let watchalert = 0
-    //     // let syncAlert = 0
-    //     // user?.checkinDevices?.fitbit?.alertArray[0]?.activeState
-    //     //     ? watchalert = await db.CryonicsModel.updateOne({ _id: req.params._id }, { $set: { "checkinDevices.fitbit.alertArray.0.activeState": false } }).exec()
-    //     //     : console.log("not setting alert array to false. either doesnt exist, or is already false")
-
-    //     // user?.checkinDevices?.fitbit?.alertArray[0]?.activeState
-    //     //     ? syncAlert = await db.CryonicsModel.updateOne({ _id: req.params._id }, { $set: { "checkinDevices.fitbit.alertArray.0.syncAlertArray": false } }).exec()
-    //     //     : console.log("not setting alert array to false. either doesnt exist, or is already false")
-
-    //     // console.log("watchalert", watchalert)
-    //     // console.log("syncAlert", syncAlert)
-    //     return res.json(user )
-    // },
-    putClearFBAlert: function (req, res) {
+    putClearFBAlert: async function (req, res) {
+        let user = 0
         console.log("putClearFBAlert req.params._id", req.params._id)
-        db.CryonicsModel
-            .updateOne({ _id: req.params._id },
-                {
-                    $set: {
-                        "checkinDevices.fitbit.syncAlertArray.$.activeState": false,
-                        "checkinDevices.fitbit.alertArray.$.activeState": false,
-                    }
-                }
-            )
-            .then(dbModelDataResult => res.json(dbModelDataResult))
-            .catch(err => res.status(422).json(err));
+        try {
+            user = await db.CryonicsModel.findOne({ _id: req.params._id }).exec()
+        } catch (err) {
+            return res.status(400).json({
+                error: 'Error en STATUS2'
+            })
+        }
+        console.log("putClearFBAlert   --------  user.name", user.name)
+        let watchalert = 0
+        let syncAlert = 0
+        user?.checkinDevices?.fitbit?.alertArray[0]?.activeState
+            ? watchalert = await db.CryonicsModel.updateOne({ _id: req.params._id }, { $set: { "checkinDevices.fitbit.alertArray.0.activeState": false } }).exec()
+            : console.log("not setting alert array to false. either doesnt exist, or is already false")
+
+        user?.checkinDevices?.fitbit?.alertArray[0]?.activeState
+            ? syncAlert = await db.CryonicsModel.updateOne({ _id: req.params._id }, { $set: { "checkinDevices.fitbit.alertArray.0.syncAlertArray": false } }).exec()
+            : console.log("not setting alert array to false. either doesnt exist, or is already false")
+
+        console.log("watchalert", watchalert)
+        console.log("syncAlert", syncAlert)
+        return res.json({ watchalert, syncAlert } )
     },
+    // putClearFBAlert: function (req, res) {
+    //     console.log("putClearFBAlert req.params._id", req.params._id)
+    //     db.CryonicsModel
+    //         .updateOne({ _id: req.params._id },
+    //             {
+    //                 $set: {
+    //                     "checkinDevices.fitbit.syncAlertArray.$.activeState": false,
+    //                     "checkinDevices.fitbit.alertArray.$.activeState": false,
+    //                 }
+    //             }
+    //         )
+    //         .then(dbModelDataResult => res.json(dbModelDataResult))
+    //         .catch(err => res.status(422).json(err));
+    // },
 };
 
