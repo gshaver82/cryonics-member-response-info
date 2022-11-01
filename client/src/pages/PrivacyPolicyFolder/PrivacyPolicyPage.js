0cce7bdcf2b01eecafb4bb9749d3d698897acb67
gshaver <geneshaver@gmail.com>
2022-10-31 21:30:40 -0500

    Update PrivacyPolicyPage.js

--- client/src/pages/PrivacyPolicyFolder/PrivacyPolicyPage.js
+++ client/src/pages/PrivacyPolicyFolder/PrivacyPolicyPage.js
@@ -6,10 +6,13 @@ function PrivacyPolicy() {
     return (
         <div>
 
-            
+
             <h1>PrivacyPolicy</h1>
 
-            
+            <p>
+                the autoheartwatch app will not gather any user data for any reason at this time. 
+            </p>
+
         </div>
     );
 }
