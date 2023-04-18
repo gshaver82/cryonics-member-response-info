8164ba03497581b75a66e629cbe7a8e6efe11898
gshaver <geneshaver@gmail.com>
2023-04-18 18:01:18 -0500

    Update PrivacyPolicyPage.js

--- client/src/pages/PrivacyPolicyFolder/PrivacyPolicyPage.js
+++ client/src/pages/PrivacyPolicyFolder/PrivacyPolicyPage.js
@@ -5,13 +5,47 @@ function PrivacyPolicy() {
 
     return (
         <div>
+            <h2>Introduction</h2>
+            <p>AutoHeartWatch ("we", "our", or "us") is committed to protecting the privacy of our users. This Privacy Policy ("Policy") describes how we collect, use, and disclose your personal information when you use the AutoHeartWatch mobile application ("App"). By using the App, you agree to the collection and use of your personal information in accordance with this Policy.</p>
 
+            <h2>Information We Collect</h2>
+            <p>When you use our App, we may collect the following types of information:</p>
+            <ol>
+                <li>Personal Information: We collect personal information that you provide to us, such as your name, phone number, and emergency contact details.</li>
+                <li>Device Information: We may collect information about your device, such as the device type, operating system, and unique device identifiers.</li>
+                <li>Location Information: With your permission, we collect GPS location data to provide location-based services and share your location with your emergency contacts when necessary.</li>
+                <li>Usage Information: We may collect information about your usage of the App, including alarm times, interactions with the App, and your response to notifications.</li>
+            </ol>
 
-            <h1>PrivacyPolicy</h1>
+            <h2>How We Use Your Information</h2>
+            <p>We use the information we collect for the following purposes:</p>
+            <ol>
+                <li>To provide and maintain the App, including the delivery of notifications, text messages, and phone calls at the specified alarm times.</li>
+                <li>To detect, prevent, and address technical issues with the App.</li>
+                <li>To monitor the usage and performance of the App, as well as to improve its functionality.</li>
+                <li>To share your location with your emergency contacts in case you do not respond to the notifications and alerts within the given time frame.</li>
+                <li>To provide customer support and assistance.</li>
+            </ol>
 
-            <p>
-                the autoheartwatch app will not gather any user data for any reason at this time. 
-            </p>
+            <h2>Information Sharing and Disclosure</h2>
+            <p>We may share your personal information with third parties in the following circumstances:</p>
+            <ol>
+                <li>With your emergency contacts, when you do not respond to the notifications and alerts within the given time frame.</li>
+                <li>With service providers who help us provide the App's features, such as SMS and calling services.</li>
+                <li>As required by law, in response to a subpoena, court order, or other legal process.</li>
+                <li>In connection with a merger, acquisition, or sale of our assets, provided that the acquiring party agrees to protect your personal information in accordance with this Policy.</li>
+            </ol>
+
+            <h2>Security</h2>
+            <p>We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure. However, no method of transmission or electronic storage is completely secure, and we cannot guarantee the absolute security of your personal information.</p>
+
+            <h2>Your Choices</h2>
+            <p>You may update or delete your personal information in the App at any time. You can also manage the permissions for the App, such as disabling GPS location access, through your device's settings. Please note that disabling certain permissions may limit the functionality of the App.</p>
+
+            <h2>Changes to This Policy</h2>
+            <p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the updated Policy on the App. It is your responsibility to review this Policy periodically for any changes.</p>
+
+            <p>By using the AutoHeartWatch App, you consent to the terms of this Privacy Policy.</p>
 
         </div>
     );
